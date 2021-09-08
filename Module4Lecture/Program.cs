aca9006c7d0e4e99c0700b8447558043930e1786
Andrew <andrewmgunn26@gmail.com>
2021-09-08 13:10:33 -0500

    running total in while loop

--- Module4Lecture/Program.cs
+++ Module4Lecture/Program.cs
@@ -4,31 +4,52 @@ namespace Module4Lecture
 {
     class Program
     {
-        static void Main(string[] args)
-        {
 
+        public void RunningTotal()
+        {
+            int total =0;
+            int amount = 0;
+
+            Console.Write("Enter Sales Amount");
+            Int32.TryParse(Console.ReadLine(), out amount);
+
+            while (amount != -1)
+            {
+                total += amount;
+                Console.Write("Enter Sales Amount");
+                Int32.TryParse(Console.ReadLine(), out amount);
+            }
+
+            Console.WriteLine($"Your total is {total:C}");
+        }
+        
+        public void FirstTryItOut()
+        {
             /* loop displays even numbers between 2 and 10 incldue 2 and 10
              loop displays all numbers between 11 and 34 includiong 11 & 34
              loop displays all number between 56 and 3, inclusive, counting backwwards
-             */
-            
-            /*
+        
              * if i is already created you can do for (i;-;-;) or for (; -; -;)
              */
-            for (int i = 2; i <= 10; i+= 2)
+            for (int i = 2; i <= 10; i += 2)
             {
                 Console.WriteLine(i);
             }
-            
             for (int i = 11; i <= 34; i++)
             {
                 Console.WriteLine(i);
             }
-            
             for (int i = 56; i >= 3; i--)
             {
                 Console.WriteLine(i);
             }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //p.FirstTryItOut();
+            p.RunningTotal();
         }
     }
 }
