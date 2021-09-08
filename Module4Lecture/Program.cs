1efc9534d7876c92dd7ab9fc3fc4b71a9fe83444
Andrew <andrewmgunn26@gmail.com>
2021-09-08 12:42:38 -0500

    for loop

--- Module4Lecture/Program.cs
+++ Module4Lecture/Program.cs
@@ -7,9 +7,27 @@ namespace Module4Lecture
         static void Main(string[] args)
         {
 
-            for (int i = 2; i <= 10; i++)
+            /* loop displays even numbers between 2 and 10 incldue 2 and 10
+             loop displays all numbers between 11 and 34 includiong 11 & 34
+             loop displays all number between 56 and 3, inclusive, counting backwwards
+             */
+            
+            /*
+             * if i is already created you can do for (i;-;-;) or for (; -; -;)
+             */
+            for (int i = 2; i <= 10; i+= 2)
             {
-                
+                Console.WriteLine(i);
+            }
+            
+            for (int i = 11; i <= 34; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+            for (int i = 56; i >= 3; i--)
+            {
+                Console.WriteLine(i);
             }
         }
     }
